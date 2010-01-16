1870e64009d95e0dfae468fb30359d13df42e5c2
David Kobia <david@ushahidi.com>
2010-01-16 15:09:33 -0500

    * Google Maps, Google Satellite and OSM layer integration

--- application/hooks/settings.php
+++ application/hooks/settings.php
@@ -71,18 +71,8 @@ $default_map = $settings->default_map;
 $api_google = $settings->api_google;
 $api_yahoo = $settings->api_yahoo;
 
-if ($default_map == 2) {		// Virtual Earth / Live
-	Kohana::config_set('settings.api_url', '<script src="http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6"></script>');
-}
-elseif ($default_map == 3) {	// Yahoo
-	Kohana::config_set('settings.api_url', '<script type="text/javascript" src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=' . $api_yahoo . '"></script>');
-}
-elseif ($default_map == 4) {	// Open Streetmaps
-	Kohana::config_set('settings.api_url', '<script src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"></script>');
-}
-else {							// Google
-	Kohana::config_set('settings.api_url', '<script src="http://maps.google.com/maps?file=api&v=2&key=' . $api_google . '" type="text/javascript"></script>');
-}
+// Google/OSM Combo
+Kohana::config_set('settings.api_url', '<script src="http://maps.google.com/maps?file=api&v=2&key=' . $api_google . '" type="text/javascript"></script><script src="http://www.openstreetmap.org/openlayers/OpenStreetMap.js"></script>');
 
 // And in case you want to display all maps on one page...
 Kohana::config_set('settings.api_url_all', '<script src="http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6"></script><script type="text/javascript" src="http://api.maps.yahoo.com/ajaxymap?v=3.0&appid=' . $api_yahoo . '"></script><script src="http://maps.google.com/maps?file=api&v=2&key=' . $api_google . '" type="text/javascript"></script>'.html::script('media/js/OpenStreetMap'));
