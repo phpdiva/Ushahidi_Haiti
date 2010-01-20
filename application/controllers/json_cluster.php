e136687e160c2df1bc281129c4faf9c8e21de5f3
David Kobia <david@ushahidi.com>
2010-01-20 01:00:47 -0500

    * Resovlved issues with page caching, clustering and large queries that were killing the DB

--- application/controllers/json_cluster.php
+++ application/controllers/json_cluster.php
@@ -14,6 +14,9 @@
  * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
  */
 
+define('OFFSET', 268435456);
+define('RADIUS', 85445659.4471); /* $offset / pi() */
+
 class Json_Cluster_Controller extends Template_Controller
 {
     public $auto_render = TRUE;
@@ -36,7 +39,6 @@ class Json_Cluster_Controller extends Template_Controller
 		// Database
 		$db = new Database();
 		
-		$cache = "";
 		$json = "";
         $json_item = "";
         $json_array = array();
@@ -48,7 +50,8 @@ class Json_Cluster_Controller extends Template_Controller
 		$zoomLevel = (isset($_GET['z']) && !empty($_GET['z'])) ?
 			$_GET['z'] : 8;
 			
-		$distance = (10000000 >> $zoomLevel) / 100000;	
+		//$distance = 60;
+		$distance = (10000000 >> $zoomLevel) / 100000;
 		
 		// Category ID
 		$category_id = (isset($_GET['c']) && !empty($_GET['c']) && 
@@ -98,152 +101,123 @@ class Json_Cluster_Controller extends Template_Controller
 			}
 		}
 		
-		
-		// Do we have Database cached JSON?
-		$query_json = $db->query("SELECT `json` FROM `json` WHERE zoom=$zoomLevel AND category_id=$category_id AND 
-			start_date=$start_date AND end_date=$end_date AND southwest='$southwest' AND northeast='$northeast'");
-		
-		foreach ($query_json as $json_cache)
-		{
-			$cache = $json_cache->json;
-		}
-		
-		// Cache exists
-		if ($cache)
-		{
-			$json = $cache;
-		}
-		else
-		{ // No Cache
 			
-	//		$query = ORM::factory('incident')
-	//			->select('incident.*, location.latitude, location.longitude, incident_category.category_id')
-	//			->join('location', 'location.id', 'incident.location_id','INNER')
-	//			->join('incident_category', 'incident.id', 'incident_category.incident_id','INNER')
-	//			->join('category', 'category.id', 'incident_category.category_id','INNER')
-	//			->where('incident.incident_active=1'.$filter)
-	//			->find_all();
+//		$query = ORM::factory('incident')
+//			->select('incident.*, location.latitude, location.longitude, incident_category.category_id')
+//			->join('location', 'location.id', 'incident.location_id','INNER')
+//			->join('incident_category', 'incident.id', 'incident_category.incident_id','INNER')
+//			->join('category', 'category.id', 'incident_category.category_id','INNER')
+//			->where('incident.incident_active=1'.$filter)
+//			->find_all();
 
-			$query = $db->query("SELECT DISTINCT `incident`.id, `location`.`latitude`, `location`.`longitude` FROM `incident` INNER JOIN `location` ON (`location`.`id` = `incident`.`location_id`) INNER JOIN `incident_category` ON (`incident`.`id` = `incident_category`.`incident_id`) INNER JOIN `category` ON (`incident_category`.`category_id` = `category`.`id`) WHERE incident.incident_active=1 $filter ORDER BY `incident`.`id` ASC ");	
-			$query->result(FALSE, MYSQL_ASSOC);
-	//		echo count($query);
+		$query = $db->query("SELECT DISTINCT `incident`.id, `location`.`latitude`, `location`.`longitude` FROM `incident` INNER JOIN `location` ON (`location`.`id` = `incident`.`location_id`) INNER JOIN `incident_category` ON (`incident`.`id` = `incident_category`.`incident_id`) INNER JOIN `category` ON (`incident_category`.`category_id` = `category`.`id`) WHERE incident.incident_active=1 $filter ORDER BY `incident`.`id` ASC ");	
+		$query->result(FALSE, MYSQL_ASSOC);
+//		echo count($query);
 
-			//*** There has to be a more efficient way to do this than to
-			// create a whole other array - to be examined later
-			$markers = array();
-			foreach ($query as $row)
-			{
-				$markers[] = $row;
-			}
-
-			$clusters = array();	// Clustered
-			$singles = array();		// Non Clustered
-
-			// Loop until all markers have been compared
-			while (count($markers))
-			{
-				$marker  = array_pop($markers);
-				$cluster = array();
-
-				// Compare marker against all remaining markers.
-				foreach ($markers as $key => $target)
-				{
-					// This function returns the distance between two markers, at a defined zoom level.
-					$pixels = abs($marker['longitude']-$target['longitude']) + 
-						abs($marker['latitude']-$target['latitude']);
-					// echo $pixels."<BR>";
-					// If two markers are closer than defined distance, remove compareMarker from array and add to cluster.
-					if ($pixels < $distance)
-					{
-						unset($markers[$key]);
-						$cluster[] = $target;
-					}
-				}
-
-				// If a marker was added to cluster, also add the marker we were comparing to.
-				if (count($cluster) > 0)
-				{
-					$cluster[] = $marker;
-					$clusters[] = $cluster;
-				}
-				else
-				{
-					$singles[] = $marker;
-				}
-			}
-
-			//print_r($clusters);
-			//print_r($singles);
-
-			// Create Json
-			foreach ($clusters as $cluster)
-			{
-				// Calculate cluster center
-				$cluster_center = $this->_calculateCenter($cluster);
-				
-				// Save Cluster to DB
-				$cluster_latlon = explode(",", $cluster_center);
-				$save_cluster = ORM::factory('cluster');
-				$save_cluster->latitude = $cluster_latlon[1];
-				$save_cluster->longitude = $cluster_latlon[0];
-				$save_cluster->cluster_count = count($cluster);
-				$save_cluster->save();
-				
-				// Link Incident to This Cluster
-				foreach ($cluster as $marker)
-				{				
-					$save_marker = ORM::factory('cluster_incident');
-					$save_marker->cluster_id = $save_cluster->id;
-					$save_marker->incident_id = $marker['id'];
-					$save_marker->save();
-				}
-
-				$json_item = "{";
-			    $json_item .= "\"type\":\"Feature\",";
-			    $json_item .= "\"properties\": {";
-				$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/index/". $save_cluster->id ."/>" . count($cluster) . " Reports</a>")) . "\",";			
-			    $json_item .= "\"category\":[0], ";
-				$json_item .= "\"color\": \"".$color."\", ";
-				$json_item .= "\"icon\": \"".$icon."\", \n";
-			    $json_item .= "\"timestamp\": \"0\", ";
-				$json_item .= "\"count\": \"" . count($cluster) . "\"";
-			    $json_item .= "},";
-			    $json_item .= "\"geometry\": {";
-			    $json_item .= "\"type\":\"Point\", ";
-			    $json_item .= "\"coordinates\":[" . $cluster_center . "]";
-			    $json_item .= "}";
-			    $json_item .= "}";
-
-			    array_push($json_array, $json_item);
-			}
-
-			foreach ($singles as $single)
-			{
-				$json_item = "{";
-			    $json_item .= "\"type\":\"Feature\",";
-			    $json_item .= "\"properties\": {";
-				$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/view/" . $marker['id'] . "/>1 Report</a>")) . "\",";	
-			    $json_item .= "\"category\":[0], ";
-				$json_item .= "\"color\": \"".$color."\", ";
-				$json_item .= "\"icon\": \"".$icon."\", \n";
-			    $json_item .= "\"timestamp\": \"0\", ";
-				$json_item .= "\"count\": \"" . 1 . "\"";
-			    $json_item .= "},";
-			    $json_item .= "\"geometry\": {";
-			    $json_item .= "\"type\":\"Point\", ";
-			    $json_item .= "\"coordinates\":[" . $single['longitude'] . ", " . $single['latitude'] . "]";
-			    $json_item .= "}";
-			    $json_item .= "}";
-
-			    array_push($json_array, $json_item);
-			}
-
-			$json = implode(",", $json_array);
-
-			// Cache JSON results
-			$db->query("INSERT INTO `json` (`json`,`zoom`,`category_id`,`start_date`,`end_date`,`southwest`,`northeast`)
-			 	VALUES ('$json', '$zoomLevel', '$category_id', '$start_date', '$end_date', '$southwest', '$northeast') ");
+		//*** There has to be a more efficient way to do this than to
+		// create a whole other array - to be examined later
+		$markers = array();
+		foreach ($query as $row)
+		{
+			$markers[] = $row;
 		}
+
+		$clusters = array();	// Clustered
+		$singles = array();		// Non Clustered
+
+		// Loop until all markers have been compared
+		while (count($markers))
+		{
+			$marker  = array_pop($markers);
+			$cluster = array();
+
+			// Compare marker against all remaining markers.
+			foreach ($markers as $key => $target)
+			{
+				// This function returns the distance between two markers, at a defined zoom level.
+				//$pixels = $this->_pixelDistance($marker['latitude'], $marker['longitude'], 
+				//	$target['latitude'], $target['longitude'], $zoomLevel);
+				
+				$pixels = abs($marker['longitude']-$target['longitude']) + 
+					abs($marker['latitude']-$target['latitude']);
+				// echo $pixels."<BR>";
+				// If two markers are closer than defined distance, remove compareMarker from array and add to cluster.
+				if ($pixels < $distance)
+				{
+					unset($markers[$key]);
+					$target['distance'] = $pixels;
+					$cluster[] = $target;
+				}
+			}
+
+			// If a marker was added to cluster, also add the marker we were comparing to.
+			if (count($cluster) > 0)
+			{
+				$cluster[] = $marker;
+				$clusters[] = $cluster;
+			}
+			else
+			{
+				$singles[] = $marker;
+			}
+		}
+
+		//print_r($clusters);
+		//print_r($singles);
+
+		// Create Json
+		foreach ($clusters as $cluster)
+		{
+			// Calculate cluster center
+			$bounds = $this->_calculateCenter($cluster);
+			$cluster_center = $bounds['center'];
+			$southwest = $bounds['sw'];
+			$northeast = $bounds['ne'];
+			
+			// Number of Items in Cluster
+			$cluster_count = count($cluster);
+			
+			$json_item = "{";
+		    $json_item .= "\"type\":\"Feature\",";
+		    $json_item .= "\"properties\": {";
+			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/index/?sw=".$southwest."&ne=".$northeast.">" . $cluster_count . " Reports</a>")) . "\",";			
+		    $json_item .= "\"category\":[0], ";
+			$json_item .= "\"color\": \"".$color."\", ";
+			$json_item .= "\"icon\": \"".$icon."\", ";
+		    $json_item .= "\"timestamp\": \"0\", ";
+			$json_item .= "\"count\": \"" . $cluster_count . "\"";
+		    $json_item .= "},";
+		    $json_item .= "\"geometry\": {";
+		    $json_item .= "\"type\":\"Point\", ";
+		    $json_item .= "\"coordinates\":[" . $cluster_center . "]";
+		    $json_item .= "}";
+		    $json_item .= "}";
+
+		    array_push($json_array, $json_item);
+		}
+
+		foreach ($singles as $single)
+		{
+			$json_item = "{";
+		    $json_item .= "\"type\":\"Feature\",";
+		    $json_item .= "\"properties\": {";
+			$json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href=" . url::base() . "reports/view/" . $marker['id'] . "/>1 Report</a>")) . "\",";	
+		    $json_item .= "\"category\":[0], ";
+			$json_item .= "\"color\": \"".$color."\", ";
+			$json_item .= "\"icon\": \"".$icon."\", ";
+		    $json_item .= "\"timestamp\": \"0\", ";
+			$json_item .= "\"count\": \"" . 1 . "\"";
+		    $json_item .= "},";
+		    $json_item .= "\"geometry\": {";
+		    $json_item .= "\"type\":\"Point\", ";
+		    $json_item .= "\"coordinates\":[" . $single['longitude'] . ", " . $single['latitude'] . "]";
+		    $json_item .= "}";
+		    $json_item .= "}";
+
+		    array_push($json_array, $json_item);
+		}
+
+		$json = implode(",", $json_array);
 
 		//header('Content-type: application/json');
 		$this->template->json = $json;
@@ -288,7 +262,7 @@ class Json_Cluster_Controller extends Template_Controller
 				$json_item = "{";
 	            $json_item .= "\"type\":\"Feature\",";
 	            $json_item .= "\"properties\": {";
-				$json_item .= "\"id\": \"".$row->id."\", \n";
+				$json_item .= "\"id\": \"".$row->id."\", ";
 	            $json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href='" . url::base() . "reports/view/" . $row->id . "'>" . htmlentities($row->incident_title) . "</a>")) . "\",";
 				$json_item .= "\"category\":[0], ";
 	            $json_item .= "\"timestamp\": \"" . strtotime($row->incident_date) . "\"";
@@ -305,7 +279,7 @@ class Json_Cluster_Controller extends Template_Controller
 			$json_item = "{";
             $json_item .= "\"type\":\"Feature\",";
             $json_item .= "\"properties\": {";
-			$json_item .= "\"id\": \"".$marker->id."\", \n";
+			$json_item .= "\"id\": \"".$marker->id."\", ";
             $json_item .= "\"name\":\"" . str_replace(chr(10), ' ', str_replace(chr(13), ' ', "<a href='" . url::base() . "reports/view/" . $marker->id . "'>" . htmlentities($marker->incident_title) . "</a>")) . "\",";
 			$json_item .= "\"category\":[0], ";
             $json_item .= "\"timestamp\": \"" . strtotime($marker->incident_date) . "\"";
@@ -393,38 +367,92 @@ class Json_Cluster_Controller extends Template_Controller
 	}
 	
 	
+	private function _lonToX($lon)
+	{
+	    return round(OFFSET + RADIUS * $lon * pi() / 180);        
+	}
+
+	private function _latToY($lat)
+	{
+	    return round(OFFSET - RADIUS * 
+	                log((1 + sin($lat * pi() / 180)) / 
+	                (1 - sin($lat * pi() / 180))) / 2);
+	}
+
 	private function _pixelDistance($lat1, $lon1, $lat2, $lon2, $zoom)
 	{
-		$x1 = $lon1;
-		$y1 = $lat1;
-		$x2 = $lon2;
-		$y2 = $lat2;
+	    $x1 = $this->_lonToX($lon1);
+	    $y1 = $this->_latToY($lat1);
 
-		//return sqrt(pow(($x1-$x2),2) + pow(($y1-$y2),2)) >> (21 - $zoom); //21 is the max zoom level
-		return ($x1-$x2) + ($y1-$y2) >> (21 - $zoom);
-		
-//	    $x1 = $lon1;
-//	    $y1 = $lat1;
-//	    $x2 = $lon2;
-//	    $y2 = $lat2;
-//
-//		return sqrt(pow(($x1-$x2),2) + pow(($y1-$y2),2)) >> (21 - $zoom); //21 is the max zoom level
+	    $x2 = $this->_lonToX($lon2);
+	    $y2 = $this->_latToY($lat2);
+
+	    return sqrt(pow(($x1-$x2),2) + pow(($y1-$y2),2)) >> (21 - $zoom);
 	}
 	
 	
     private function _calculateCenter($cluster)
 	{
-        // Calculate average lat and lon of clustered items
+		// Calculate average lat and lon of clustered items
+		$south = 0;
+		$west = 0;
+		$north = 0;
+		$east = 0;
+		
         $lat_sum = $lon_sum = 0;
         foreach ($cluster as $marker)
 		{
+			if (!$south)
+			{
+				$south = $marker['latitude'];
+			}
+			elseif ($marker['latitude'] < $south)
+			{
+				$south = $marker['latitude'];
+			}
+				
+			if (!$west)
+			{
+				$west = $marker['longitude'];
+			}
+			elseif ($marker['longitude'] < $west)
+			{
+				$west = $marker['longitude'];
+			}
+			
+			if (!$north)
+			{
+				$north = $marker['latitude'];
+			}
+			elseif ($marker['latitude'] > $north)
+			{
+				$north = $marker['latitude'];
+			}
+			
+			if (!$east)
+			{
+				$east = $marker['longitude'];
+			}
+			elseif ($marker['longitude'] > $east)
+			{
+				$east = $marker['longitude'];
+			}
+			
 			$lat_sum += $marker['latitude'];
 			$lon_sum += $marker['longitude'];
         }
 		$lat_avg = $lat_sum / count($cluster);
 		$lon_avg = $lon_sum / count($cluster);
 		
-		return $lon_avg.", ".$lat_avg;
+		$center = $lon_avg.",".$lat_avg;
+		$sw = $west.",".$south;
+		$ne = $east.",".$north;
+		
+		return array(
+			"center"=>$center,
+			"sw"=>$sw,
+			"ne"=>$ne
+		);
     }
 	
 	
